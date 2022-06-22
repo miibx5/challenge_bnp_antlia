96d3a037f7898f7f143b60bbae3a8a3a99ad4879
Éder Costa <miibx5@gmail.com>
2022-06-22 16:29:15 -0300

    feat: Creating the ProductCosif table and the entities that represent the table along with the repository and integration tests

--- backend/src/test/kotlin/br/com/edersystems/backend/builders/ProdutoCosifPKBuilder.kt
+++ backend/src/test/kotlin/br/com/edersystems/backend/builders/ProdutoCosifPKBuilder.kt
@@ -1,7 +1,7 @@
 /*
 ...........................................................
 Project .....................: challenge_bnp_antlia
-Creation Date ...............: 22/06/2022 12:46:35
+Creation Date ...............: 22/06/2022 15:02:04
 Developer....................: eder
 Copyright....................: 2022
 Codification.................: UTF-8
@@ -9,9 +9,16 @@ Codification.................: UTF-8
  Éder L. Costa - © Copyright 2022 - All Rights Reserved
 ...........................................................
 */
-package br.com.edersystems.backend.configuration.sql
+package br.com.edersystems.backend.builders
 
-enum class Commands(val commandSql: String) {
-	CLEAR_PRODUCT_COSIF_TABLE("DELETE FROM PRODUTO_COSIF"),
-	CLEAR_PRODUCT_TABLE("DELETE FROM PRODUTO")
+import br.com.edersystems.backend.infrastructure.entities.ProdutoCosifPK
+import java.util.UUID
+
+object ProdutoCosifPKBuilder {
+	lateinit var codProduto: UUID
+		private set
+
+	fun build() = ProdutoCosifPK(codProduto = codProduto)
+
+	fun withCodProduct(codProduto: UUID) = apply { this.codProduto = codProduto }
 }
